ef3b0aeb6c4541ed9b433711dc2c01c7f5f8e231
ricklegac <rick.legac@gmail.com>
2021-12-08 22:33:35 -0300

    cv

--- 02-baseangular/src/app/pages/posts/posts.component.ts
+++ 02-baseangular/src/app/pages/posts/posts.component.ts
@@ -9,16 +9,17 @@ import { DataService } from '../../services/data.service';
 export class PostsComponent implements OnInit {
 
 
-  mensajes: any[] = [];
+  mensajes: any;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void { //es donde la pagina es cargada por primera vez 
-    this.dataService.getPosts() //llama la instruccion que esta en data.service.ts el return de getPost 
-    .subscribe((posts: any) => { // aca cambiamos el post: any[]
-      console.log(posts);
-      this.mensajes = posts;
-    });
+   // console.log('mensaje log');
+  this.mensajes= this.dataService.getPosts() //llama la instruccion que esta en data.service.ts el return de getPost 
+    //.subscribe((posts: any) => { // aca cambiamos el post: any[]
+    //  console.log(posts);
+     // this.mensajes = posts;
+    //});
   }
 
 }
--- 02-baseangular/src/app/services/data.service.ts
+++ 02-baseangular/src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { tap } from 'rxjs/operators'; // accion cuando se recibe algun 
 @Injectable({
   providedIn: 'root' //los servicios funcionan de manera global en toda la aplicacion, eso significa root 
 })
@@ -9,6 +10,9 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getPosts(){  
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+    .pipe(
+      tap (console.log)
+    );
   }
 }
